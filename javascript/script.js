52862ac8e20aa8a5cde2472c86d5abdf2b8aa398
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-04-26 13:12:05 +0200

    Create updateVariables() for display and calculations

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,6 @@
+let number1 = '', number2 = '', operand = '';
+let calculation = '';
+
 // Functions for operations logic
 function add(num1, num2) {
     return num1 + num2;
@@ -47,4 +50,64 @@ function hover() {
     });
 }
 
+// Update variables
+function updateVariables() {
+    let operandPressed = false;
+    let equalPressed = false;
+
+    const buttons = document.querySelectorAll('.button');
+    const displayEntered = document.querySelector('.entered');
+    const displayCalculation = document.querySelector('.calculation');
+
+    const classNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero'];
+    const classOperands = ['divide', 'multiply', 'minus', 'plus'];
+    const classSpecial = ['equal', 'period', 'clear', 'delete'];
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (classNumbers.some(element => button.classList.contains(element))) {
+                if (!operandPressed) {
+                    number1 += button.textContent;
+                    calculation = number1 + ' ';
+                    displayEntered.textContent = number1;
+                }
+                else if (operandPressed) {
+                    number2 += button.textContent;
+                    displayCalculation.textContent = calculation;
+                    displayEntered.textContent = number2;
+                }
+            }
+            else if (classOperands.some(element => button.classList.contains(element))) {
+                if (number1 === '') {
+                    alert('Please enter number before operand.');
+                    return;
+                }
+                else if (operandPressed === true) {
+                    alert('Operand already present');
+                    return;
+                }
+
+                operand = button.textContent;
+                displayCalculation.textContent = calculation;
+                calculation += operand + ' ';
+                displayEntered.textContent = operand;
+
+                operandPressed = true;
+            }
+            else if (classSpecial.some(element => button.classList.contains(element))) {
+                if (button.classList.contains('equal') && operandPressed === true && number2 != '') {
+                    calculation += number2;
+                    number1 = eval(calculation);
+                    number2 = '';
+                    calculation = number1 + ' ';
+                    displayEntered.textContent = number1;
+                    displayCalculation.textContent = '';
+                    operandPressed = false;
+                }
+            }
+        });
+    });
+}
+
 hover();
+updateVariables();
